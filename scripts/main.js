e81e3b2990a70dfb4fb4fdd00429a4631a37c053
robalty <32559243+robalty@users.noreply.github.com>
2018-04-21 13:41:36 -0700

    New script
    new main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,34 @@
-window.onLoad = function(){
-  if (!Notification) {
-    alert('Notifications aren\'t supported by this browser!.'); 
-    return;
-  }
+window.onload = function(){
+    menuNav('one');
+    if (!Notification) {
+        alert('Notifications aren\'t supported by this browser!.');
+        return;
+    }
 
-  if (Notification.permission !== "granted")
-    Notification.requestPermission();
+    if (Notification.permission !== "granted")
+        Notification.requestPermission();
+
 };
 
 function notifyMe() {
-  if (Notification.permission !== "granted")
-    Notification.requestPermission();
-  else {
-    var notification = new Notification('PSU Campus Resources', {
-      icon: './img/icon.png',
-      body: "Hi there! Are you having a good day at PSU?",
-    });
-    notification.onclick = function () {
-      window.open("http://10.200.118.205:8887/");      
-    };
-  }
+    if (Notification.permission !== "granted")
+        Notification.requestPermission();
+    else {
+        var notification = new Notification('PSU Campus Resources', {
+            icon: './img/icon.png',
+            body: "Hi there! Are you having a good day at PSU?",
+        });
+        notification.onclick = function () {
+            window.open("http://10.200.118.205:8887/");
+        };
+    }
+}
+
+function menuNav(to){
+    var divs = document.getElementsByTagName("div");
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].style.display = 'none';
+    }
+    var toShow = document.getElementById(to);
+    toShow.style.display = "block";
 }
